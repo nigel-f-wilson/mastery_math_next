73a8c4587e7035efc5f3772af1069f2d4b188ca0
nolastemgarden <nolastemgarden@gmail.com>
2022-07-13 08:48:02 -0500

    Copy /navigation from CRA version

---
+++ components/navigation/Navbar.js
@@ -0,0 +1,46 @@
+import React, { useContext, useState } from 'react'
+
+// THIRD PARTY
+import { Box, AppBar , IconButton } from '@mui/material';
+
+// ICONS
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
+
+// CUSTOM
+import { SettingsContext, LayoutContext } from '../../contexts';
+import { MenuDrawer } from "./";
+
+export default function Navbar(props) {
+  const [drawerOpen, setDrawerOpen] = useState(false)
+
+  const { colorTheme } = useContext(SettingsContext)
+  const { navbarHeightPx } = useContext(LayoutContext)
+
+  const bgColor = (colorTheme === "dark") ? "darkGrey" : "lightGrey"
+  const fontColor = (colorTheme === "dark") ? "white" : "black"
+
+  const toggleDrawer = (event) => {
+    if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+      return;
+    }
+    setDrawerOpen((prev) => !prev)
+  };
+
+  return (
+    <AppBar position='sticky' >
+      <Box height={`${navbarHeightPx}px`} bgcolor={bgColor} display='flex' justifyContent='end' alignItems='center' paddingX='0.5rem' >
+        <IconButton 
+          onClick={toggleDrawer}
+          children={<FontAwesomeIcon icon={faBars} color={fontColor} />}
+          aria-controls="main-menu"
+          aria-haspopup="true"
+        />
+        <MenuDrawer 
+          drawerOpen={drawerOpen}
+          toggleDrawer={toggleDrawer}
+        />
+      </Box>
+    </AppBar>
+  )
+}
