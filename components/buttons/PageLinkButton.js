5ce871ed2a6636705917502d141e1cf0c4a7252d
nolastemgarden <nolastemgarden@gmail.com>
2022-07-13 09:34:22 -0500

    Update PageLinkButton.js

--- components/buttons/PageLinkButton.js
+++ components/buttons/PageLinkButton.js
@@ -2,19 +2,33 @@ import React from "react";
 import Link from 'next/link'
 
 // THIRD PARTY
+import { Button, Typography } from '@mui/material';
 // ICONS
 import { faArrowCircleRight } from '@fortawesome/free-solid-svg-icons'
 
 // INTERNAL
-import { ButtonWrapper, ButtonLabel } from "./";
 
 export default function PageLinkButton(props) {
-  const { to, text } = props
+  const { to, label } = props
   return (
     <Link to={to} style={{ textDecoration: 'none', width: "100%" }} >
-      <ButtonWrapper id="link-to-page-button-wrapper" >
-        <ButtonLabel text={text} endIcon={faArrowCircleRight} />
-      </ButtonWrapper>
+      <Button 
+          endIcon={faArrowCircleRight}
+          sx={{ border: border, height: '2.7rem', width: '90%'}}
+        >
+          <Typography 
+            color={fontColor}
+            sx={{
+              textTransform: 'none',
+              fontWeight: 400,
+              // letterSpacing: '2px',
+              fontSize: '1.4rem',
+              lineHeight: '2.0rem',
+            }}
+          >
+            {label}
+          </Typography>
+        </Button>
     </Link>
   );
 };
