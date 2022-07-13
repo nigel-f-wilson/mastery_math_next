18ea5ba5c2e39ca269f777cc6a6049da0c5ab071
nolastemgarden <nolastemgarden@gmail.com>
2022-07-13 08:52:22 -0500

    Install and configure Font Awesome

--- contexts/SettingsContext.js
+++ contexts/SettingsContext.js
@@ -3,6 +3,24 @@ import React, { useState, createContext } from "react";
 export const SettingsContext = createContext();
 export const SettingsContextProvider = props => {
   // COLOR THEME
+  const lightTheme = {
+    name: "light",
+    fontColor: "#000",
+    bgColor: "#FFF",
+    bgAccentColor: "#DDD",
+  }
+  const darkTheme = {
+    name: "dark",
+    fontColor: "#FFF",
+    bgColor: "#000",
+    bgAccentColor: "#222",
+  }
+  const [colorTheme, setColorTheme] = useState(darkTheme)  // or {lightTheme}  
+  function toggleColorTheme(event) {
+    event.preventDefault()
+    setColorTheme((prev) => (prev === lightTheme) ? darkTheme : lightTheme)
+  }
+  
   const settingsContext = {
     colorTheme,
     toggleColorTheme,
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,12 @@
 import '../styles/globals.css'
 
+import { config } from '@fortawesome/fontawesome-svg-core'
+import '@fortawesome/fontawesome-svg-core/styles.css'
+config.autoAddCss = false
+
+// App Context
+import { SettingsContextProvider, LayoutContextProvider } from '../contexts';
+
 function MyApp({ Component, pageProps }) {
   return (
     <SettingsContextProvider >
