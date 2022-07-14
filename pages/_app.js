dfd213e50e62d57b36bdb24febbb7c3f6eca82d1
nolastemgarden <nolastemgarden@gmail.com>
2022-07-14 11:15:31 -0500

    Follow Getting Started steps
    Followed instructions on [Vercel's Getting Started Page[(https://next-auth.js.org/getting-started/example)

--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,28 @@
 import '../styles/globals.css'
 
+// FONT AWESOME
 import { config } from '@fortawesome/fontawesome-svg-core'
 import '@fortawesome/fontawesome-svg-core/styles.css'
 config.autoAddCss = false
 
+// NEXT AUTH
+import { SessionProvider } from "next-auth/react"
+
 // App Context
 import { SettingsContextProvider, LayoutContextProvider } from '../contexts';
 
-function MyApp({ Component, pageProps }) {
+function MyApp({
+  Component,
+  pageProps: { session, ...pageProps },
+}) {
   return (
-    <SettingsContextProvider >
-      <LayoutContextProvider >
-        <Component {...pageProps} />
-      </LayoutContextProvider>
-    </SettingsContextProvider>
+    <SessionProvider session={session}>
+      <SettingsContextProvider >
+        <LayoutContextProvider >
+          <Component {...pageProps} />
+        </LayoutContextProvider>
+      </SettingsContextProvider>
+    </SessionProvider>
   )
 }
 
